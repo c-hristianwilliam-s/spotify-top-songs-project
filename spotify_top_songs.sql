f89ae0cd19c01395e699072bd2afdc9ac4e66ff8
Christian Williams <89479044+chrxstixnn@users.noreply.github.com>
2025-01-10 11:23:21 -0800

    Changed file

---
+++ spotify_top_songs.sql
@@ -0,0 +1,53 @@
+--TABLE CREATED
+CREATE TABLE Spotify_final_dataset(
+	"Position" NUMERIC,
+	"Artist Name" TEXT,
+	"Song Name" TEXT,
+	"Days" NUMERIC,
+	"Top 10(xTimes)" NUMERIC,
+	"Peak Position" NUMERIC,
+	"Peak Position (xTimes)" NUMERIC,
+	"Peak Streams" NUMERIC,
+	"Total Streams" NUMERIC
+)
+
+--How many artists have top songs?
+SELECT COUNT(DISTINCT "Artist Name") FROM "Spotify_final_dataset";
+
+--Songs that hit #1 and how many times it hit #1
+SELECT "Artist Name", "Song Name", "Peak Position (xTimes)" FROM "Spotify_final_dataset"
+WHERE "Peak Position" = 1;
+
+--Which song had the most streams
+SELECT "Artist Name", "Song Name", "Total Streams" FROM "Spotify_final_dataset"
+ORDER BY "Total Streams" DESC LIMIT 1;
+
+--Which song had the least streams
+SELECT "Artist Name", "Song Name", "Total Streams" FROM "Spotify_final_dataset"
+ORDER BY "Total Streams" ASC LIMIT 1;
+
+--Which song had the most peak streams
+SELECT "Artist Name", "Song Name", "Peak Streams" FROM "Spotify_final_dataset"
+ORDER BY "Peak Streams" DESC LIMIT 1;
+
+--Which song had the least peak streams
+SELECT "Artist Name", "Song Name", "Peak Streams" FROM "Spotify_final_dataset"
+ORDER BY "Peak Streams" ASC LIMIT 1;
+
+--Which artists had multiple songs in the top 10.
+SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset"
+WHERE "Top 10(xTimes)" > 1 GROUP BY "Artist Name"
+ORDER BY "Artist Name";
+
+--Which song was in the top 10 the most amount of times
+SELECT "Song Name" FROM "Spotify_final_dataset" ORDER BY "Top 10(xTimes)" DESC LIMIT 1;
+
+--Which song was in the top 10 the least amount of times
+SELECT "Song Name" FROM "Spotify_final_dataset" ORDER BY "Top 10(xTimes)" ASC LIMIT 1;
+
+--Which of the songs at the top has been out for the longest
+SELECT "Artist Name", "Song Name", "Days" FROM "Spotify_final_dataset" 
+ORDER BY "Days" DESC LIMIT 1;
+
+
+
