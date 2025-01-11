491f811e7f76b4b1c040bd4b36605bc9a31fdd4d
Christian Williams <89479044+chrxstixnn@users.noreply.github.com>
2025-01-11 09:54:40 -0800

    Update spotify_top_songs.sql

--- spotify_top_songs.sql
+++ spotify_top_songs.sql
@@ -11,18 +11,41 @@ CREATE TABLE Spotify_final_dataset(
 	"Total Streams" NUMERIC
 )
 
+--Information on the artists 
+
+--How many artists have top songs?
+SELECT COUNT(DISTINCT "Artist Name") FROM "Spotify_final_dataset";
+
+--Which artist has the most songs in the top songs list
+SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset" 
+GROUP BY "Artist Name"
+ORDER BY COUNT("Artist Name") DESC LIMIT 1;
+
+--Which artist has the least songs in the top songs list
+SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset" 
+GROUP BY "Artist Name"
+ORDER BY COUNT("Artist Name") ASC LIMIT 1;
+
+--Which artists had multiple songs in the top 10.
+SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset"
+WHERE "Top 10(xTimes)" > 1 GROUP BY "Artist Name"
+ORDER BY "Artist Name";
+
+
+
+
+
+
+--Information on the songs 
+
 --How many songs in this data set
 SELECT COUNT(*) FROM "Spotify_final_dataset";
 
 --Top 50 songs ever
 SELECT "Song Name", "Artist Name" FROM "Spotify_final_dataset" LIMIT 50;
 
---How many artists have top songs?
-SELECT COUNT(DISTINCT "Artist Name") FROM "Spotify_final_dataset";
-
 --Total streams
 SELECT SUM("Total Streams") FROM "Spotify_final_dataset";
-
 
 --Songs that hit #1 and how many times it hit #1
 SELECT "Artist Name", "Song Name", "Peak Position (xTimes)" FROM "Spotify_final_dataset"
@@ -44,20 +67,6 @@ ORDER BY "Peak Streams" DESC LIMIT 1;
 SELECT "Artist Name", "Song Name", "Peak Streams" FROM "Spotify_final_dataset"
 ORDER BY "Peak Streams" ASC LIMIT 1;
 
---Which artist has the most songs in the top songs list
-SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset" 
-GROUP BY "Artist Name"
-ORDER BY COUNT("Artist Name") DESC LIMIT 1;
-
---Which artist has the least songs in the top songs list
-SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset" 
-GROUP BY "Artist Name"
-ORDER BY COUNT("Artist Name") ASC LIMIT 1;
-
---Which artists had multiple songs in the top 10.
-SELECT "Artist Name", COUNT("Artist Name") FROM "Spotify_final_dataset"
-WHERE "Top 10(xTimes)" > 1 GROUP BY "Artist Name"
-ORDER BY "Artist Name";
 
 --Which song was in the top 10 the most amount of times
 SELECT "Song Name" FROM "Spotify_final_dataset" ORDER BY "Top 10(xTimes)" DESC LIMIT 1;
@@ -84,5 +93,11 @@ SELECT AVG("Days") FROM "Spotify_final_dataset";
 
 --How many songs have been in the top 10
 SELECT COUNT("Top 10(xTimes)") FROM "Spotify_final_dataset" WHERE "Top 10(xTimes)" > 0.0;
+
+--What songs have over 200,000,000 streams
+SELECT "Song Name", "Total Streams" FROM "Spotify_final_dataset" WHERE "Total Streams" > 200000000;
+
+--What songs have under 1 million streams
+SELECT "Song Name", "Total Streams" FROM "Spotify_final_dataset" WHERE "Total Streams" < 1000000;
 
 
